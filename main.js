1cae04426cd7097a6ea82928a414f1c13b022fa8
trickjonhgamer <trickjonhgamer@gmail.com>
2025-07-30 16:40:36 -0300

    cria estilo do tabuleiro

--- main.js
+++ main.js
@@ -7,7 +7,5 @@ document.body.append(eTabuleiro);
 function criaTabuleiro(){
     const novoTabuleiro = document.createElement('div');
     novoTabuleiro.classList.add('tabuleiro');
-    novoTabuleiro.style.backgroundColor = "#7FFFD4";
-    novoTabuleiro.style.minHeight = "25px";
     return novoTabuleiro;
 }
