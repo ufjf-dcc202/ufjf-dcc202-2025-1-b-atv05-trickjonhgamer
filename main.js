38eddf07ec09de905f8111b8a7dbb2dbf50094c1
trickjonhgamer <trickjonhgamer@gmail.com>
2025-07-30 16:35:56 -0300

    adiciona cria tabuleiro

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const eH1 = document.querySelector('h1');
 eH1.textContent = "Olá Mundo!";
+
+const eTabuleiro = criaTabuleiro();
+document.body.append(eTabuleiro);
+
+function criaTabuleiro(){
+    const novoTabuleiro = document.createElement('div');
+    novoTabuleiro.classList.add('tabuleiro');
+    novoTabuleiro.style.backgroundColor = "#7FFFD4";
+    novoTabuleiro.style.minHeight = "25px";
+    return novoTabuleiro;
+}
